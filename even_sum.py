a34eb31b39707d3d8a2fcf3a041ecd684a9a261b
Joshua <72043974+Joshuwat12@users.noreply.github.com>
2022-09-19 19:20:20 +0000

    Update even_sum.py

--- even_sum.py
+++ even_sum.py
@@ -1,3 +1,17 @@
 # even_sum.py
-#
-# 
+
+
+# Input
+a = int(input("Enter an integer: "))
+b = int(input("Enter another integer, ideally greater than the first one: "))
+
+# Computation
+total = 0
+n = a + 1
+while n < b:
+  if n % 2 == 0:
+    total += n
+  n += 1
+
+# Output
+print("The sum of all even integers from", a, "to", b, "is", total)
