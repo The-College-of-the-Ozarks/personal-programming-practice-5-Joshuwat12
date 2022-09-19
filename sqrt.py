b3a3df0aec4a4c8f74f26f12d070de5de84c75e9
Joshua <72043974+Joshuwat12@users.noreply.github.com>
2022-09-19 19:47:33 +0000

    Update sqrt.py

--- sqrt.py
+++ sqrt.py
@@ -1,3 +1,20 @@
 # sqrt.py
-#
-# 
+
+
+# Input
+n = float(input("Enter a nonnegative value: "))
+while n < 0:
+  n = float(input("I SAID NONNEGATIVE: "))
+epsilon = float(input("Enter another nonnegative value: "))
+while epsilon < 0:
+  epsilon = float(input("I SAID NONNEGATIVE: "))
+
+# Computation
+lastGuess = n
+x = n/2
+while abs(lastGuess - x) > epsilon:
+  lastGuess = x
+  x = (x + n/x) / 2
+
+# Output
+print("By approximation, the square root of", n, "is", x)
